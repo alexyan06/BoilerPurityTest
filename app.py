5b549f15127243c43ac943ad4c0492ecfb1502d0
alexyan06 <yan517@purdue.edu>
2025-04-02 13:28:56 -0400

    commit

--- app.py
+++ app.py
@@ -5,10 +5,7 @@ import os
 
 app = Flask(__name__)
 
-if os.environ.get("RENDER"):
-    DATABASE = '/data/scores.db'  # On Render's persistent disk
-else:
-    DATABASE = 'scores.db'        # Local fallback
+DATABASE = 'scores.db'       # Local fallback
 
 def init_db():
     # Create the database and table if they do not exist.
